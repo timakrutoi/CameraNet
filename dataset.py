7de5a4e5e4be00051c92baac9aca871ff1a004d4
Timofei <timaoleinik@gmail.com>
2023-04-30 16:00:51 +0300

    Added things

---
+++ dataset.py
@@ -0,0 +1,144 @@
+from os import listdir, sep
+import torch
+import random
+
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+from skimage import io
+
+
+def random_crop(data, crop_size):
+    x = torch.randint(low=0, high=data.shape[0] - crop_size, size=(1,))
+    y = torch.randint(low=0, high=data.shape[1] - crop_size, size=(1,))
+
+    slice_x = slice(x, x + crop_size)
+    slice_y = slice(y, y + crop_size)
+
+    data = data[slice_x, slice_y, :]
+
+    return data
+
+
+def random_flip(x, flip_mode):
+    f = []
+    
+    if 'h' in flip_mode and torch.randint(low=0, high=2, size=(1,)):
+        f.append(1)
+
+    if 'v' in flip_mode and torch.randint(low=0, high=2, size=(1,)):
+        f.append(0)
+
+    x = x.flip(dims=f)
+
+    return x
+
+
+class S7DatasetCN(Dataset):
+    def __init__(self, directory, mode, factor,
+                 crop_size, flip):
+        self.directory = directory
+
+        self.crop_size = crop_size
+        self.flip = flip
+
+        self.dirs = listdir(self.directory)
+        random.seed(1234)
+        random.shuffle(self.dirs)
+        
+        # :C i didnt make a mid pic for this
+        for i in range(9):
+            self.dirs.remove(f'20161109_224805_{i}')
+        self.fname = 'medium_exposure'
+        
+        tmp_len = len(self.dirs)
+
+        if mode == 'train':
+            self.len = int(tmp_len * factor)
+            self.dirs = self.dirs[:self.len]
+        elif mode == 'test':
+            self.len = tmp_len - int(tmp_len * factor)
+            self.dirs = self.dirs[self.len:]
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+
+        in_ = sep.join([self.directory, self.dirs[idx],
+                        self.fname + '.bmp'])
+        mid = sep.join([self.directory, self.dirs[idx],
+                        self.fname + '.png'])
+        out = sep.join([self.directory, self.dirs[idx],
+                        self.fname + '.jpg'])
+
+        in_ = torch.tensor(io.imread(in_).astype('float'))
+        mid = torch.tensor(io.imread(mid).astype('float'))
+        out = torch.tensor(io.imread(out).astype('float'))
+#         print(in_.min(), in_.max())
+#         print(mid.min(), mid.max())
+#         print(out.min(), out.max())
+        
+#         norm = lambda x: (x / 128) - 1
+        norm = lambda x: (x / 255)
+        (in_, mid, out) = map(norm, (in_, mid, out))
+#         print(in_.min(), in_.max())
+#         print(mid.min(), mid.max())
+#         print(out.min(), out.max())
+#         print('='*40)
+
+        if self.crop_size is not None:
+            (in_, mid, out) = map(random_crop, (in_, mid, out), [self.crop_size]*3)
+
+        if self.flip is not None:
+            (in_, mid, out) = map(random_flip, (in_, mid, out), [self.flip]*3)
+
+        (in_, mid, out) = map(lambda x: x.permute(2, 0, 1), (in_, mid, out))
+
+        return (in_, mid, out)
+    
+
+def get_data(data_path, num_workers=0,
+             batch_size=1, factor=0.9,
+             crop_size=256, flip='hv'):
+
+    # [TODO] get rid of those long args
+    train_data = S7DatasetCN(
+        directory=data_path,
+        mode='train',
+        factor=factor,
+        crop_size=crop_size,
+        flip=flip
+    )
+
+    test_data = S7DatasetCN(
+        directory=data_path,
+        mode='test',
+        factor=factor,
+        crop_size=crop_size,
+        flip=flip
+    )
+
+    train_loader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers
+    )
+    test_loader = DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
+    )
+
+    return train_loader, test_loader
+
+
+if __name__ == '__main__':
+    path = '/toleinik/data/S7-ISP-new'
+    train, test = get_data(path)
+
+    for i in train:
+        print(i)
+        break
--- network.py
+++ network.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from kornia.color import rgb_to_xyz, xyz_to_rgb
+
+from myutils import *
 
 
 class RestoreNetBlock(nn.Module):
@@ -22,6 +25,10 @@ class RestoreNetBlock(nn.Module):
 
     def forward(self, x):
         return self.block(x)
+
+    def to(self, device):
+        for d in self.block:
+            d.to(device)
 
 
 class EnhanceNetBlock(nn.Module):
@@ -44,8 +51,12 @@ class EnhanceNetBlock(nn.Module):
 
     def forward(self, x):
         # skip connections
-        x = self.block[0](x) + x.clone()
-        return self.block[1:](x)
+        x2 = self.block[0](x)
+        return self.block[1:](x2) + x
+
+    def to(self, device):
+        for d in self.block:
+            d.to(device)
 
 
 class GlobalComponent(nn.Module):
@@ -55,13 +66,23 @@ class GlobalComponent(nn.Module):
 
         self.lin = nn.Sequential()
         self.lin.append(nn.Linear(n, n))
+        self.lin.append(nn.LeakyReLU())
         self.lin.append(nn.Linear(n, n))
-        self.lin.append(nn.Linear(n, n))
+        self.lin.append(nn.LeakyReLU())
+#         self.lin.append(nn.Linear(n, n))
 
     def forward(self, x):
-        x = self.pool(x)
-        b, c, _, _ = x.shape
-        return self.lin(x.reshape(b, c))
+        y = self.pool(x)
+        b, c, _, _ = y.shape
+        y = self.lin(y.view(b, c))
+        r = torch.einsum('bcij,bc->bcij', x, y.view(b, c))
+        r = self.lin[-1](r)
+        return r
+
+    def to(self, device):
+        self.pool.to(device)
+        for p in self.lin:
+            p.to(device)
 
 
 class UNetBlock(nn.Module):
@@ -75,69 +96,153 @@ class UNetBlock(nn.Module):
     def forward(self, x):
         return self.sb(self.fb(x))
 
+    def to(self, device):
+        self.fb.to(device)
+        self.sb.to(device)
+
+
+class UNetDownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, block=RestoreNetBlock):
+        super(UNetDownBlock, self).__init__()
+
+        self.conv_pool = nn.Sequential(
+            block(in_channels, out_channels),
+            nn.MaxPool2d(2),
+#             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+#             nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.conv_pool(x)
+
+    def to(self, device):
+        for m in self.conv_pool:
+            m.to(device)
+
+
+class UNetUpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, block=RestoreNetBlock):
+        super(UNetUpBlock, self).__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
+        self.block = block(in_channels, out_channels)
+            
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is BCHW
+#         diffY = x2.size()[2] - x1.size()[2]
+#         diffX = x2.size()[3] - x1.size()[3]
+
+#         x11 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+#                         diffY // 2, diffY - diffY // 2])
+#         x = torch.cat([x2, x11], dim=1)
+        return self.block(x1)
+
+    def to(self, device):
+        self.up.to(device)
+        self.block.to(device)
+
 
 class UNet(nn.Module):
     def __init__(self, block):
         super(UNet, self).__init__()
-        dims = [3, 32, 64, 128, 256, 512]
+        dims = [32, 64, 128, 256, 512]
+        dils = [1,  2,  2,   4,   8]
 
-        self.down = nn.Sequential()
+        self.std = SpaceToDepth(2)
+        self.dts = DepthToSpace(2)
+
+        self.down = nn.Sequential(
+            nn.Conv2d(12, 32, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
         for i, j in zip(dims[:-1], dims[1:]):
-            self.down.append(UNetBlock(first_block=block(i, j),
-                                      second_block=nn.MaxPool2d(2)))
-
-        self.mid = block(in_c=512, out_c=512)
+#             self.down.append(UNetBlock(first_block=block(i, j),
+#                                        second_block=nn.MaxPool2d(2)))
+            self.down.append(UNetDownBlock(i, j))
 
         # GlobalComponent
-        self.global_component = GlobalComponent(n=512)
+        self.global_component = GlobalComponent(n=dims[-1])
 
-        dils = [1, 2, 2, 4, 8]
         self.up = nn.Sequential()
         for i, j, k in zip(dims[:0:-1], dims[-2::-1], dils):
-            self.up.append(UNetBlock(dilation=k,
-                                     first_block=nn.Upsample(scale_factor=2),
-                                     second_block=block(i, j)))
+#             self.up.append(UNetBlock(first_block=nn.ConvTranspose2d(i, i, kernel_size=2, stride=2),
+#                                      second_block=block(i, j)))
+            self.up.append(UNetUpBlock(i, j))
+
+        self.end = nn.Sequential()
+        self.end.append(nn.Conv2d(32, 12, kernel_size=1, padding=0))
+        self.end.append(nn.ReLU())
 
     def forward(self, x):
-        # weird system with lists,
-        # but i didnt come up with anything better
+        # weird system with lists
+        x = self.std(x)
         d = [x]
         for i in self.down:
             d.append(i(d[-1]))
+#             print(f'down {d[-1].shape=}')
 
         m = self.global_component(d[-1])
-        # ???
-        m = torch.einsum('bcij,bc->bcij', self.mid(d[-1]), m)
+#         m = d[-1]
 
         u = [m]
         for idx, i in enumerate(self.up):
             # skip connections
-            a = i(u[-1])
-            print(a.shape, d[-idx - 2].shape)
-            u.append(a + d[-idx - 2])
+#             tmp = i(u[-1])
+#             print(f'up s {u[-1].shape} + {d[-idx - 2].shape}')
+#             u.append(tmp + d[-idx - 2])
+            u.append(i(u[-1], d[-idx - 2]))
+#             print(f'up {u[-1].shape=}')
+        
+        r = self.end(u[-1])
 
-        return u[-1]
+        return self.dts(r)
 
+    def to(self, device):
+        self.global_component.to(device)
+        for p in self.down:
+            p.to(device)
+        for p in self.up:
+            p.to(device)
+
+
+class Wrapper(nn.Module):
+    def __init__(self, f):
+        super(Wrapper, self).__init__()
+        self.f = f
+    
+    def forward(self, x):
+        return self.f(x)
+            
 
 class CameraNet(nn.Module):
     def __init__(self):
         super(CameraNet, self).__init__()
 
         # RGB to XYZ transform
-        self.rgb2xyz = rgb_to_xyz
-        self.restore = UNet(block=RestoreNetBlock)
+#         self.rgb2xyz = Wrapper(rgb_to_xyz)
+        self.restore = nn.Sequential(
+            Wrapper(rgb_to_xyz),
+            UNet(block=RestoreNetBlock)
+        )
         # XYZ to RGB transform
-        self.xyz2rgb = xyz_to_rgb
-        self.enhance = UNet(block=RestoreNetBlock)
+#         self.xyz2rgb = Wrapper(xyz_to_rgb)
+#         self.enhance = UNet(block=RestoreNetBlock)
+        self.enhance = nn.Sequential(
+            Wrapper(xyz_to_rgb),
+            UNet(block=EnhanceNetBlock)
+        )
 
     def forward(self, x):
-        x = self.rgb2xyz(x)
         x = self.restore(x)
 
-        x = self.xyz2rgb(x)
         x = self.enhance(x)
 
         return x
+
+    def to(self, device):
+        self.restore[1].to(device)
+        self.enhance[1].to(device)
 
 
 if __name__ == '__main__':
