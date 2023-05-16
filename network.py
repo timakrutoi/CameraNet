3348edb423a2bc70a9620aa5d27451bbcfd55f34
Timofei <timaoleinik@gmail.com>
2023-05-16 20:33:17 +0300

    Update network.py

--- network.py
+++ network.py
@@ -130,12 +130,12 @@ class UNetUpBlock(nn.Module):
     def forward(self, x1, x2):
         x1 = self.up(x1)
         # input is BCHW
-#         diffY = x2.size()[2] - x1.size()[2]
-#         diffX = x2.size()[3] - x1.size()[3]
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
 
-#         x11 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-#                         diffY // 2, diffY - diffY // 2])
-#         x = torch.cat([x2, x11], dim=1)
+        x11 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x11], dim=1)
         return self.block(x1)
 
     def to(self, device):
